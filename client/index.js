8d076e487aa4fe4a6aedf078577096a8dd70493c
shariandabre <dabresharian@gmail.com>
2023-03-13 09:03:02 +0530

    commit1

--- client/index.js
+++ client/index.js
@@ -1,55 +1,65 @@
-
-
 const sock = io();
-sock.on('loser', function() {
+sock.on("loser", function (word) {
   Notify({
-    title: "You lost the round!",
-    type: "warning",
+    title: `You lost the round! The word was ${word}`,
+    type: "danger",
     position: "top center",
     duration: 2000,
   });
 });
-sock.on('reload-page', function() {
-  setTimeout(function() {
-    location.reload();
-  }, 2000);
-});
+
+function joinRoom(){
+  const codeInput = document.getElementById("code").value;
+
+
+  sock.emit("code",codeInput)
+
+  window.location.href = "/";
+}
 
 document.addEventListener("DOMContentLoaded", () => {
+  sock.on("msg", (text) =>
+    Notify({
+      title: text,
+      position: "top center",
+      duration: 2000,
+    })
+  );
+  sock.on("word", (word, words) => {
+    console.log(word);
 
-  sock.on('msg',(text)=>( Notify({
-    title: text,
-    position: "top center",
-    duration: 2000,
-  })))
+    function getTileColor(letter, index) {
+      const iscorrectLetter = word.includes(letter);
+      buttons = document.getElementsByTagName("button");
 
-  let guessedWords = [[]];
-  let availableSpace = 1;
+      if (!iscorrectLetter) {
+        for (i = 0; i < 28; i++) {
+          if (
+            document
+              .getElementsByTagName("button")
+              [i].getAttribute("data-key") === letter
+          )
+            document.getElementsByTagName("button")[i].style.background =
+              "rgb(58, 58, 60)";
+        }
+        return "rgb(58, 58, 60)";
+      }
 
-  sock.on('word',(word,words)=>{
-    console.log(word)
-  let guessedWordCount = 0;
-  const keys = document.querySelectorAll(".keyboard-row button");
-  function getCurrentWordArr() {
-    const noOfGuessedWords = guessedWords.length;
-    return guessedWords[noOfGuessedWords - 1];
-  }
+      const letterInThatPosition = word.charAt(index);
+      const isCorrectPosition = letter === letterInThatPosition;
 
-  function updateGuessedWords(letter) {
-    const CurrentWordArr = getCurrentWordArr();
-    if (CurrentWordArr && CurrentWordArr.length < 5) {
-      CurrentWordArr.push(letter);
-      const availableSpaceEl = document.getElementById(String(availableSpace));
-      availableSpace = availableSpace + 1;
-      availableSpaceEl.textContent = letter;
-    }
-  }
-
-  function getTileColor(letter, index) {
-    const iscorrectLetter = word.includes(letter);
-    buttons = document.getElementsByTagName("button");
-
-    if (!iscorrectLetter) {
+      if (isCorrectPosition) {
+        for (i = 0; i < 28; i++) {
+          if (
+            document
+              .getElementsByTagName("button")
+              [i].getAttribute("data-key") === letter
+          )
+            document.getElementsByTagName("button")[i].style.background =
+              "rgb(83, 141, 78)";
+        }
+        return "rgb(83, 141, 78)";
+      }
       for (i = 0; i < 28; i++) {
         if (
           document
@@ -57,152 +67,326 @@ document.addEventListener("DOMContentLoaded", () => {
             [i].getAttribute("data-key") === letter
         )
           document.getElementsByTagName("button")[i].style.background =
-            "rgb(58, 58, 60)";
+            "rgb(181, 159, 59)";
       }
-      return "rgb(58, 58, 60)";
+      return "rgb(181, 159, 59)";
     }
 
-    const letterInThatPosition = word.charAt(index);
-    const isCorrectPosition = letter === letterInThatPosition;
+    function getEnemyTileColor(letter, index) {
+      const iscorrectLetter = word.includes(letter);
 
-    if (isCorrectPosition) {
-      for (i = 0; i < 28; i++) {
-        if (
-          document
-            .getElementsByTagName("button")
-            [i].getAttribute("data-key") === letter
-        )
-          document.getElementsByTagName("button")[i].style.background =
-            "rgb(83, 141, 78)";
+      if (!iscorrectLetter) {
+        return "rgb(58, 58, 60)";
       }
-      return "rgb(83, 141, 78)";
+
+      const letterInThatPosition = word.charAt(index);
+      const isCorrectPosition = letter === letterInThatPosition;
+
+      if (isCorrectPosition) {
+        return "rgb(83, 141, 78)";
+      }
+
+      return "rgb(181, 159, 59)";
     }
-    for (i = 0; i < 28; i++) {
+    sock.on("currentWordArr", function (currentWordArr) {
+      gameState.Enemyword = currentWordArr;
+      const firstLetterId = 100 * 5;
+      const interval = 200;
+      currentWordArr.forEach((letter, index) => {
+        setTimeout(() => {
+          const tileColor = getEnemyTileColor(letter, index);
+          const letterId = firstLetterId + index;
+          const letterEl = document.getElementById(letterId);
+          letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
+        }, interval * index);
+      });
+      saveGameState();
+    });
+    sock.on("correct", function (count) {
+      if(count <=6){
+      gameState.Enemycount = count;
+      document.getElementById("correct").innerText = count ;
+      saveGameState();
+    }
+    });
+    function EnemyGuess() {
+      if (gameState.Enemycount !== undefined) {
+        document.getElementById("correct").innerText = gameState.Enemycount;
+      }
       if (
-        document.getElementsByTagName("button")[i].getAttribute("data-key") ===
-        letter
-      )
-        document.getElementsByTagName("button")[i].style.background =
-          "rgb(181, 159, 59)";
+        gameState.Enemyword !== undefined &&
+        gameState.Enemyword.length === 5
+      ) {
+        const firstLetterId = 100 * 5;
+        const interval = 200;
+        gameState.Enemyword.forEach((letter, index) => {
+          setTimeout(() => {
+            const tileColor = getEnemyTileColor(letter, index);
+            const letterId = firstLetterId + index;
+            const letterEl = document.getElementById(letterId);
+            letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
+          }, interval * index);
+        });
+      }
     }
-    return "rgb(181, 159, 59)";
-  }
-
-  //------------Delete----------------------------
-  function handleDeleteLetter() {
-    const currentWordArr = getCurrentWordArr();
-    const removedLetter = currentWordArr.pop();
-
-    guessedWords[guessedWords.length - 1] = currentWordArr;
-
-    const lastLetterEl = document.getElementById(String(availableSpace - 1));
-    if (lastLetterEl.getAttribute("data-empty") === "true") {
-      lastLetterEl.textContent = "";
-      availableSpace = availableSpace - 1;
-    }
-  }
-  //-----------------------------------------------
-
-  //------------enter------------------------------
-  function handleSubmitWord() {
-    const currentWordArr = getCurrentWordArr();
-    if (currentWordArr.length !== 5) {
-      Notify({
-        title: "Not enough letters",
-        type: "warning",
-        position: "top center",
-        duration: 2000,
+    function resetEnemyGuess() {
+      document.getElementById("correct").innerText = 0;
+      if (
+        gameState.Enemyword !== undefined &&
+        gameState.Enemyword.length === 5
+      ) {
+      const firstLetterId = 100 * 5;
+      const interval = 200;
+      gameState.Enemyword.forEach((letter, index) => {
+        setTimeout(() => {
+          const letterId = firstLetterId + index;
+          const letterEl = document.getElementById(letterId);
+          letterEl.style = `background-color:#111;border-color:#3a3a3c`;
+        }, interval * index);
       });
+      return
+    }
+    }
+    function resettiles() {
+      for (i = 0; i <= gameState.guessedWords.length; i++) {
+        const firstLetterId = i * 5 + 1;
+        const interval = 200;
+        if (
+          gameState.guessedWords[i] !== undefined &&
+          gameState.guessedWords[i].length === 5
+        ) {
+          gameState.guessedWords[i].forEach((letter, index) => {
+            setTimeout(() => {
+              const letterId = firstLetterId + index;
+              const letterEl = document.getElementById(letterId);
+              letterEl.classList.remove("animate__flipInX");
+              letterEl.classList.add("animate__fadeIn");
+              letterEl.innerText = "";
+              letterEl.setAttribute("data-empty", true);
+              letterEl.style = `background-color:#111;border-color:#3a3a3c`;
+            }, interval * index);
+          });
+        }
+      }
+      setTimeout(() => {
+        for (i = 0; i < 28; i++) {
+          document.getElementsByTagName("button")[i].style.background =
+            "#818384";
+        }
+      }, 1000);
+      resetEnemyGuess();
+      return true
     }
 
-    const currentWord = currentWordArr.join("");
-
-    if (currentWord === word && guessedWords.length !== 6) {
-      Notify({
-        title: "Congratulations",
-        position: "top center",
-        duration: 2000,
-      });
-      sock.emit('loser');
-      sock.emit('winner') 
+    function savetiles() {
+      for (i = 0; i <= gameState.guessedWords.length; i++) {
+        const firstLetterId = i * 5 + 1;
+        const interval = 200;
+        if (
+          gameState.guessedWords[i] !== undefined &&
+          gameState.guessedWords[i].length === 5
+        ) {
+          gameState.guessedWords[i].forEach((letter, index) => {
+            setTimeout(() => {
+              const tileColor = getTileColor(letter, index);
+              const letterId = firstLetterId + index;
+              const letterEl = document.getElementById(letterId);
+              letterEl.innerText = letter;
+              letterEl.classList.add("animate__flipInX");
+              letterEl.setAttribute("data-empty", false);
+              letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
+            }, interval * index);
+          });
+        }
+        if (gameState.guessedWords[i] !== undefined) {
+          gameState.guessedWords[i].forEach((letter, index) => {
+            setTimeout(() => {
+              const letterId = firstLetterId + index;
+              const letterEl = document.getElementById(letterId);
+              letterEl.innerText = letter;
+            }, interval * index);
+          });
+        }
+      }
     }
-    if (guessedWords.length === 6) {
-      if (currentWord === word) {
+
+    let gameState = localStorage.getItem("gameState");
+    if (gameState) {
+      gameState = JSON.parse(gameState);
+      savetiles();
+      EnemyGuess();
+    } else {
+      gameState = {
+        guessedWords: [[]],
+        availableSpace: 1,
+        guessedWordCount: 0,
+        Enemyword: [],
+        Enemycount: 0,
+      };
+    }
+
+    function saveGameState() {
+      localStorage.setItem("gameState", JSON.stringify(gameState));
+    }
+    window.addEventListener("beforeunload", saveGameState);
+
+    const resetGameState = () => {
+      gameState = {
+        guessedWords: [[]],
+        availableSpace: 1,
+        guessedWordCount: 0,
+      };
+      saveGameState();
+    };
+
+    sock.on("reload-page", function () {
+      if(resettiles()){
+      resetGameState();
+      localStorage.removeItem("gameState");}
+    });
+
+    const keys = document.querySelectorAll(".keyboard-row button");
+    function getCurrentWordArr() {
+      const noOfGuessedWords = gameState.guessedWords.length;
+      return gameState.guessedWords[noOfGuessedWords - 1];
+    }
+
+    function updateGuessedWords(letter) {
+      const CurrentWordArr = getCurrentWordArr();
+      if (CurrentWordArr && CurrentWordArr.length < 5) {
+        CurrentWordArr.push(letter);
+        const availableSpaceEl = document.getElementById(
+          String(gameState.availableSpace)
+        );
+        gameState.availableSpace = gameState.availableSpace + 1;
+        availableSpaceEl.textContent = letter;
+      }
+    }
+
+    //------------Delete----------------------------
+    function handleDeleteLetter() {
+      const currentWordArr = getCurrentWordArr();
+      const removedLetter = currentWordArr.pop();
+
+      gameState.guessedWords[gameState.guessedWords.length - 1] =
+        currentWordArr;
+
+      const lastLetterEl = document.getElementById(
+        String(gameState.availableSpace - 1)
+      );
+      if (lastLetterEl.getAttribute("data-empty") === "true") {
+        lastLetterEl.textContent = "";
+        gameState.availableSpace = gameState.availableSpace - 1;
+      }
+    }
+    //-----------------------------------------------
+
+    //------------enter------------------------------
+    function handleSubmitWord() {
+      const currentWordArr = getCurrentWordArr();
+      if (currentWordArr.length !== 5) {
+        Notify({
+          title: "Not enough letters",
+          type: "warning",
+          position: "top center",
+          duration: 2000,
+        });
+      }
+
+      const currentWord = currentWordArr.join("");
+
+      if (currentWord === word && gameState.guessedWords.length !== 6) {
         Notify({
           title: "Congratulations",
           position: "top center",
           duration: 2000,
         });
-        sock.emit('loser');
-        sock.emit('winner')
-
-      } else {
-        if (words.includes(currentWord)) {
+        setTimeout(() => {
+          sock.emit("winner");
+          sock.emit("loser", word);
+        }, 1000);
+      }
+      if (gameState.guessedWords.length === 6) {
+        if (currentWord === word) {
           Notify({
-            title: `You have no more guesses! The word was ${word}`,
-            type: "warning",
-            position: "top center",
-            duration: 6000,
-          });
-        }
-        else{
-          Notify({
-            title: "Enter a new word",
-            type: "warning",
+            title: "Congratulations",
             position: "top center",
             duration: 2000,
           });
+          setTimeout(() => {
+            sock.emit("winner");
+            sock.emit("loser", word);
+          }, 1000);
+        } else {
+          if (words.includes(currentWord)) {
+            Notify({
+              title: `You have no more guesses! The word was ${word}`,
+              type: "danger",
+              position: "top center",
+              duration: 6000,
+            });
+          } else {
+            Notify({
+              title: "Enter a new word",
+              type: "warning",
+              position: "top center",
+              duration: 2000,
+            });
+          }
+        }
+      }
+
+      if (currentWord.length === 5) {
+        if (words.includes(currentWord)) {
+          sock.emit("currentWordArr", currentWordArr);
+          //--------------animations----------------------
+          const firstLetterId = gameState.guessedWordCount * 5 + 1;
+          const interval = 200;
+          currentWordArr.forEach((letter, index) => {
+            setTimeout(() => {
+              const tileColor = getTileColor(letter, index);
+              const letterId = firstLetterId + index;
+              const letterEl = document.getElementById(letterId);
+              letterEl.classList.add("animate__flipInX");
+              letterEl.setAttribute("data-empty", false);
+              letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
+            }, interval * index);
+          });
+          //-----------------------------------------------
+          gameState.guessedWordCount += 1;
+          if(gameState.guessedWordCount <= 5){
+            gameState.guessedWords.push([]);
+            saveGameState()
+          }
+          sock.emit("correct", gameState.guessedWordCount);
+        } else {
+          if (gameState.guessedWords.length !== 6) {
+            Notify({
+              title: "Enter a new word",
+              type: "warning",
+              position: "top center",
+              duration: 2000,
+            });
+          }
+        }
+      }
+    }
+
+    for (let i = 0; i < keys.length; i++) {
+      keys[i].onclick = ({ target }) => {
+        const letter = target.getAttribute("data-key");
+        if (letter === "enter") {
+          handleSubmitWord();
+          return;
         }
 
-      }
+        if (letter === "del") {
+          handleDeleteLetter();
+          return;
+        }
+
+        updateGuessedWords(letter);
+      };
     }
-
-    if (currentWord.length === 5) {
-      if (words.includes(currentWord)) {
-
-        //--------------animations----------------------
-        const firstLetterId = guessedWordCount * 5 + 1;
-        const interval = 200;
-        currentWordArr.forEach((letter, index) => {
-          setTimeout(() => {
-            const tileColor = getTileColor(letter, index);
-            const letterId = firstLetterId + index;
-            const letterEl = document.getElementById(letterId);
-            letterEl.classList.add("animate__flipInX");
-            letterEl.setAttribute("data-empty", false);
-            letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
-          }, interval * index);
-        });
-        guessedWordCount += 1;
-        //-----------------------------------------------
-        
-        guessedWords.push([]);
-      } else { if(guessedWords.length !== 6){
-        Notify({
-          title: "Enter a new word",
-          type: "warning",
-          position: "top center",
-          duration: 2000,
-        });}
-      }
-    }
-  }
-
-  for (let i = 0; i < keys.length; i++) {
-    keys[i].onclick = ({ target }) => {
-      const letter = target.getAttribute("data-key");
-      if (letter === "enter") {
-        handleSubmitWord();
-        return;
-      }
-
-      if (letter === "del") {
-        handleDeleteLetter();
-        return;
-      }
-
-      updateGuessedWords(letter);
-    };
-  }
-})
+  });
 });
--- server/server.js
+++ server/server.js
@@ -11,35 +11,66 @@ const io = socketio(server);
 
 let word = words[Math.floor(Math.random() * words.length)];
 
-app.get("/join-room", (req, res, next) => {
-  const code = req.query.code;
-  console.log(`Received code ${code} from client.`);
-  if (code === 'Code') {
-    return next();
-  } else {
-    return res.sendStatus(403);
-  }
-}, (req, res) => {
-  res.sendStatus(200);
-});
+const wordsByRoom = {};
+
 
 io.on("connection", (sock) => {
+
   console.log("Client connected");
 
-  sock.emit("msg", "you are connected");
-  io.emit("word", word, words);
-  sock.on('loser', () => {
-    // notify all clients except the loser that they lost the round
-    sock.broadcast.emit('loser');
+
+    const roomId = "axbr4";
+
+
+  if (io.sockets.adapter.rooms.size === 0 || io.sockets.adapter.rooms.values().next().value.size === 1) {
+    sock.join(roomId);
+    console.log(`Client joined room ${roomId}`);
+    sock.emit("msg", `You joined room ${roomId}`);
+
+    if (io.sockets.adapter.rooms.get(roomId).size === 2) {
+      io.to(roomId).emit("msg", "The game is starting...");
+      if (!wordsByRoom[roomId]) {
+        wordsByRoom[roomId] = words[Math.floor(Math.random() * words.length)];
+      }
+      io.to(roomId).emit("word", wordsByRoom[roomId], words);
+    }
+
+  } else {
+    sock.emit("msg", "Sorry, the room is full. Try again later.");
+    sock.disconnect(true);
+    console.log("Client refused connection: room is full");
+  }
+
+  sock.on("currentWordArr", (currentWordArr) => {
+    sock.broadcast.to(roomId).emit("currentWordArr", currentWordArr);
   });
+
+  sock.on("correct", (count) => {
+    sock.broadcast.to(roomId).emit("correct", count);
+  });
+
+  sock.on("loser", (word) => {
+    sock.broadcast.to(roomId).emit("loser",word);
+  });
+
   sock.on("winner", () => {
-    word = words[Math.floor(Math.random() * words.length)];
-    io.emit("reload-page"); 
-    io.emit("word", word, words);
+    io.to(roomId).emit("reload-page");
+    wordsByRoom[roomId] = words[Math.floor(Math.random() * words.length)];
+    console.log(wordsByRoom)
+    io.to(roomId).emit("word", wordsByRoom[roomId], words);
   });
 
   sock.on("disconnect", () => {
     console.log("Client disconnected");
+    if (io.sockets.adapter.rooms.size > 0) {
+      const rooms = io.sockets.adapter.rooms;
+      rooms.forEach((room, roomId) => {
+        if (room.has(sock.id)) {
+          io.to(roomId).emit("msg", "One of the players has left the game.");
+          console.log(`Client left room ${roomId}`);
+        }
+      });
+    }
   });
 });
 
